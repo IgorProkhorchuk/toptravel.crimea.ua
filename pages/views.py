ece5aa00112500d57a4e48aa71a08a0d48f5862e
Igor Prokhorchuk <igor.prokhorchuk@yahoo.com>
2023-08-28 22:46:45 +0300

    feat: add templates

--- pages/views.py
+++ pages/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homePageView(request):
-	return HttpResponse('Hello, World!')
+	return HttpResponse('Coming soon')
 
 	
